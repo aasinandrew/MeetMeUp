11f4f1c0082a790b99046875939ffb95457e46f9
Andrew Nguyen <anguy080@ucr.edu>
2015-07-27 12:54:37 -0700

    Initial Commit

---
+++ MeetMeUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetMeUp
+//
+//  Created by Andrew  Nguyen on 7/27/15.
+//  Copyright (c) 2015 Andrew Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetMeUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetMeUp
+//
+//  Created by Andrew  Nguyen on 7/27/15.
+//  Copyright (c) 2015 Andrew Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
